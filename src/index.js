ebc35d2df8acdc0dbcacfb39c14abcffa1ed36bb
Vainila <workmail1421@gmail.com>
2021-02-28 00:43:55 +0300

    feat: implement brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let ops = [];
+  let ends = [];
+  for(let i = 0; i< bracketsConfig.length; i++){
+    ops.push(bracketsConfig[i][0]);
+    ends.push(bracketsConfig[i][1]);
+  }
+
+  for(let symbol of str){
+    if(ops.includes(symbol) && ends.includes(symbol)){
+        if(stack[stack.length-1]==symbol){
+          stack.pop();
+        }
+        else{
+          stack.push(symbol);
+        }
+    }
+    else if (ops.includes(symbol)){
+      stack.push(symbol);
+    }
+    else{
+      if(stack[stack.length-1]==ops[ends.findIndex(cur => cur === symbol)]){
+        stack.pop();
+      }else{
+        return false;
+      }
+    }
+  }
+  return stack.length==0;
 }
